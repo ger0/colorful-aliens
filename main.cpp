e6d3f6d45726d7eb2c53a1b4351ce712ff7ce4e4
ger0 <mrgero155@gmail.com>
2022-06-10 01:29:49 +0200

    Dodanie sprzątaczy (jeszcze nie działają)

--- main.cpp
+++ main.cpp
@@ -101,17 +101,28 @@ unsigned chooseResource(unsigned offset) {
    }
 }
 
-void sendPacket(Packet_t &pkt, int destination, int tag) {
+// Wrapper na wysyłanie pakietów ------------------------------------------
+void sendPacket(Packet_t& pkt, int destination, int tag) {
     MPI_Send(&pkt, 1, MPI_PAKIET_T, destination, tag, MPI_COMM_WORLD);
 }
 
-void funcINT() {
-   debug("------------------ Zamykanie programu... ------------------");
-   isFinished = true;
-   Packet_t pkt;
-   sendPacket(pkt, rank, FINISH);
+void broadcastPacket(Packet_t& pkt, int tag) {
+   for (unsigned i = 0; i < size; i++)    sendPacket(pkt, i, tag);
 }
 
+// wysyla do wszystkich i czeka na odbior ACK
+void broadcastAndAcks(Packet_t& pkt, int tag) { 
+   pthread_mutex_lock(&acksMutex);
+   acks = 0;
+   broadcastPacket(pkt, tag);
+   /* Odbiór ACK i sortowanie kolejki w wątku komunikacyjnym
+   *  kontynuacja kiedy otrzymamy ACK od kazdego procesu jak odpowiedzą */
+   while (acks < size) {
+      pthread_cond_wait(&acksCond, &acksMutex);
+   }
+   pthread_mutex_unlock(&acksMutex);
+}
+// ------------------------------------------------------------------------
 Packet_t prepareRequest(int index) {
    return Packet_t {
       .timestamp  = timestamp, 
@@ -121,26 +132,42 @@ Packet_t prepareRequest(int index) {
    };
 }
 
-void sendRelease(int resourceID) {
+void broadcastRelease(int resourceID) {
    Packet_t rel_packet = Packet_t{
       .timestamp  = timestamp,
       .type       = process_state,
       .index      = resourceID,
       .src        = rank
    };
-   acks = 0;
 
    pthread_mutex_lock(&timestampsMutex);
    ++timestamp;
    pthread_mutex_unlock(&timestampsMutex);
 
-   for (unsigned i = 0; i < size; i++) {
-      sendPacket(rel_packet, i, RELEASE);
-   }
+   broadcastPacket(rel_packet, RELEASE);
    // TODO: odebrać ACK?
 }
 
-// sprawdzic
+// Funkcja wykonywania po uzyskaniu SIGINT
+void funcINT() {
+   debug("------------------ Zamykanie programu... ------------------");
+   isFinished = true;
+   Packet_t pkt;
+   sendPacket(pkt, rank, FINISH);
+}
+// debug print kolejki
+void debugPrintQueue(unsigned hotelID) {
+   auto& queue = queues[hotelID];
+   debug("  Kolejka dostepu do hotelu %d:", hotelID);
+   for (unsigned i = 0; i < queue.size(); i++) {
+      debug("     [%d], idx: %d, kolor: %d, timestamp: %d", 
+            queue[i].process_index, i, 
+            (int)queue[i].type, queue[i].timestamp
+      );
+   }
+}
+
+// sprawdzenie czy wszystkie procesy maja timestamp wiekszy niz requesta
 bool checkAcks(unsigned req_timestamp) {
    pthread_mutex_lock(&timestampsMutex);
    for (unsigned i = 0; i < size; i++) {
@@ -155,10 +182,12 @@ bool checkAcks(unsigned req_timestamp) {
    return true;
 }
 
+// zwraca true jeżeli obecny proces jest na szczycie kolejki żądań
 bool procAtQueueTop(std::vector<Entry>& queue) {
    return queue.front().process_index == rank;
 }
 
+// procedura wejścia do sekcji krytycznej dla zasobu - Przewodnika
 void getGuide() {
    pthread_mutex_lock(&queueMutex);
    int guideID = chooseResource(GUIDE_OFFSET);
@@ -171,22 +200,12 @@ void getGuide() {
    pthread_mutex_unlock(&timestampsMutex);
 
    Packet_t req_packet = prepareRequest(guideID);
-
-   pthread_mutex_lock(&acksMutex);
-   acks = 0;
-   for (unsigned i = 0; i < size; i++) {
-      sendPacket(req_packet, i, REQUEST_G);
-   }
-   // Odbior ACK i sortowanie kolejki w watku komunikacyjnym
-   // kontynuacja kiedy otrzymamy ACK od kazdego procesu jak odpowiedzą 
-   while (acks < size) {
-      pthread_cond_wait(&acksCond, &acksMutex);
-   }
-   pthread_mutex_unlock(&acksMutex);
+   broadcastAndAcks(req_packet, REQUEST_G);
 
    if (checkAcks(req_packet.timestamp)) {
-      // TOOD: wywalić aktywne czekanie
       pthread_mutex_lock(&queueMutex);
+      /* jezeli proces nie jest na szczycie kolejki zadan, to czekamy na otrzymanie jakiegos release 
+      *  po czym sprawdzamy czy juz znajduje sie na szczycie */
       while(!procAtQueueTop(queues[guideID])) {
          pthread_cond_wait(&queueCond, &queueMutex);
       }
@@ -197,11 +216,11 @@ void getGuide() {
       usleep(rand() % 10'000 + 2'000);
    }
    currentGuide = -99999;
-   sendRelease(guideID);
+   broadcastRelease(guideID);
 }
 
-// główna pętla dla kosmitow
-void alien_procedure() {
+// ----------------------------------- główna pętla dla kosmitow ------------------------------------
+void alienProcedure() {
    while (!isFinished) {
       //int hotelID = 0;
       // spimy randomowa dlugosc 
@@ -218,34 +237,13 @@ void alien_procedure() {
       pthread_mutex_unlock(&timestampsMutex);
 
       Packet_t req_packet = prepareRequest(hotelID);
-
-      pthread_mutex_lock(&acksMutex);
-      acks = 0;
-      for (unsigned i = 0; i < size; i++) {
-         sendPacket(req_packet, i, REQUEST_H);
-      }
-      // Odbior ACK i sortowanie kolejki w watku komunikacyjnym
-      // kontynuacja kiedy otrzymamy ACK od kazdego procesu jak odpowiedzą 
-      while (acks < size) {
-         pthread_cond_wait(&acksCond, &acksMutex);
-      }
-      pthread_mutex_unlock(&acksMutex);
+      broadcastAndAcks(req_packet, REQUEST_H);
 
       pthread_mutex_lock(&queueMutex);
       auto& queue = queues[hotelID];
-      /* sprawdzenie czy wszystkie ACK maja wiekszy timestamp niz przy wysylaniu 
-       * TODO: sprawdzić! */
+
       if (checkAcks(req_packet.timestamp)) {
-         // debug print kolejki
-         {
-            debug("  Kolejka dostepu do hotelu %d:", hotelID);
-            for (unsigned i = 0; i < queue.size(); i++) {
-               debug("     [%d], idx: %d, kolor: %d, timestamp: %d", 
-                     queue[i].process_index, i, 
-                     (int)queue[i].type, queue[i].timestamp
-               );
-            }
-         }
+         debugPrintQueue(hotelID);
          /* Sprawdzenie czy w kolejce do hotelu nie ma innego koloru przed obecnym procesem
           * jeżeli nie - proces wchodzi do hotelu */
          bool isDifferentColour = false;
@@ -272,7 +270,56 @@ void alien_procedure() {
       }
       pthread_mutex_unlock(&queueMutex);
       currentlyIn = -99999;
-      sendRelease(hotelID);
+      broadcastRelease(hotelID);
+   }
+}
+
+// ----------------------------------- główna pętla dla sprzątaczy ------------------------------------
+void cleanerProcedure() {
+   while (!isFinished) {
+      unsigned hotelID;
+
+      pthread_mutex_lock(&queueMutex);
+      std::vector<unsigned> availIDs;
+      for (unsigned i = 0; i < HOTEL_COUNT; i++) {
+         for (auto& entry: queues[i]) {
+            if (entry.type == CLEANER) {
+               break;
+            }
+            if (&entry == &queues[i].back()) {
+               availIDs.push_back(i);
+            }
+         }
+      }
+      pthread_mutex_unlock(&queueMutex);
+
+      if (availIDs.size() != 0) {
+         hotelID = rand() % availIDs.size(); 
+      } else {
+         // technicznie ten sleep nie jest potrzebny
+         /* TODO: dorobic sprawdzenie czy procesy nie przekroczyly progu wejscia do hoteli 
+         * wtedy probowac dostac sie do kolejki do hotelu */
+         usleep(rand() % 20'000);
+         continue;
+      }
+
+      Packet_t req_packet = prepareRequest(hotelID);
+      broadcastAndAcks(req_packet, REQUEST_H);
+
+      if (checkAcks(req_packet.timestamp)) {
+         pthread_mutex_lock(&queueMutex);
+         /* jezeli proces nie jest na szczycie kolejki zadan, to czekamy na otrzymanie jakiegos release 
+         *  po czym sprawdzamy czy juz znajduje sie na szczycie */
+         while(!procAtQueueTop(queues[hotelID])) {
+            pthread_cond_wait(&queueCond, &queueMutex);
+         }
+         pthread_mutex_unlock(&queueMutex);
+
+         currentlyIn = hotelID;
+      }
+      debug("CZYSZCZENIE HOTELU %d", hotelID);
+      usleep(rand() % 10'000 + 2'000);
+      broadcastRelease(hotelID);
    }
 }
 
@@ -286,14 +333,17 @@ unsigned* initTimestampsArray() {
    return point;
 }
 
-void assign_state(int& rank, int& size) {
+// funkcja przypisująca odpowiedni stan procesowi (na podstawie rank)
+void assignState() {
    // SPRZATACZ 
    if (rank < size * CLEANER_PROC / 100) {
       process_state = CLEANER;
-   } // FIOLETOWY KOSMITA
+   } 
+   // FIOLETOWY KOSMITA
    else if (rank < size * (CLEANER_PROC + RED_PROC) / 100) {
       process_state = ALIEN_RED;
-   } // BLEKITNY KOSMITA
+   } 
+   // BLEKITNY KOSMITA
    else if (rank < size * (CLEANER_PROC + RED_PROC + BLUE_PROC) / 100) {
       process_state = ALIEN_BLUE;
    }
@@ -307,7 +357,7 @@ int main(int argc, char **argv) {
    
    // tworzenie typu do komunikatu
    const int nitems = FIELDNO;
-   int       blocklengths[FIELDNO] = {2,1,1, 1};
+   int       blocklengths[FIELDNO] = {2, 1, 1, 1};
    MPI_Datatype typy[FIELDNO] = {MPI_UNSIGNED, MPI_INT, MPI_INT, MPI_INT};
 
    MPI_Aint     offsets[FIELDNO]; 
@@ -326,17 +376,23 @@ int main(int argc, char **argv) {
    srand(time(NULL) + rank);
 
    pthread_create(&commThread, NULL, startKomWatek, 0);
-   assign_state(rank, size);
+   assignState();
+   // KOSMITA
    if (process_state != CLEANER) {
-      alien_procedure();
+      alienProcedure();
+   // sprzątacz 
    } else {
+      cleanerProcedure();  
    }
    pthread_join(commThread, NULL);
 
+   /* ------ na obecną chwilę to się prawdopodbnie nie wykona ------ */
    pthread_mutex_destroy(&queueMutex);
    pthread_mutex_destroy(&timestampsMutex);
    pthread_mutex_destroy(&acksMutex);
+
    pthread_cond_destroy(&acksCond);
+   pthread_cond_destroy(&queueCond);
 
    MPI_Type_free(&MPI_PAKIET_T);
    MPI_Finalize();
